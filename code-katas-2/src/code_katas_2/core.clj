ca4944cf48552d465c7fc84018b5c7e1737d0d9a
LENOVO <LENOVO@Lenovo-PC>
2014-09-04 00:24:36 -0300

    que tiene que hacer cada funcion

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -1,10 +1,11 @@
 (ns code-katas-2.core)
 
-
 (defn unpartial
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
   [f]
+  
+  "funcion que recibe una funcion como parametro"
   )
 
 
@@ -12,7 +13,13 @@
   "Dado un numero cualquiera de secuencias, cada una ya ordenada de menor a mayor, encontrar el numero
    mas chico que aparezca en todas las secuencias, las secuencias pueden ser infinitas."
   [& seqs]
-  )
+  
+  
+  "primero tengo que ver los cabezales si son igules, si no lo son encontrar el menor"
+  "con el map first veo el cabezal de cada secuencia"
+ (map first seqs))
+  
+      
 
 
 (defn intercalar
@@ -20,6 +27,9 @@
    retorne una nueva coleccion donde el valor es insertado intercalado cada dos argumentos
    que cumplan el predicado"
   [predicado valor secuencia]
+    "recibe funcion elemento a intercalar y lista"
+  
+  
   )
 
 
